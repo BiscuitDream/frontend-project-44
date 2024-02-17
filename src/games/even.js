c88a0fb732a4e4bef854dac6714a44e692cbef3d
Igor Pechenkin <ipechenkin@gmail.com>
2024-02-17 17:23:41 +0300

    fix rename generateQuestionPredicateRandNum

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,9 @@
-import { generateQuestionPredicateRandNum, isEven } from '../tools.js';
+import { generateQuestionByPredicateRandNum, isEven } from '../tools.js';
 import game from '../index.js';
 
 const evenDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const evenQuestion = () => generateQuestionPredicateRandNum(0, 100, isEven);
+const evenQuestion = () => generateQuestionByPredicateRandNum(0, 100, isEven);
 
 const runEven = () => game(evenDescription, evenQuestion);
 
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,9 +1,9 @@
-import { generateQuestionPredicateRandNum, isPrime } from '../tools.js';
+import { generateQuestionByPredicateRandNum, isPrime } from '../tools.js';
 import game from '../index.js';
 
 const primeDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const primeQuestion = () => generateQuestionPredicateRandNum(2, 100, isPrime);
+const primeQuestion = () => generateQuestionByPredicateRandNum(2, 100, isPrime);
 
 const runGcd = () => game(primeDescription, primeQuestion);
 
